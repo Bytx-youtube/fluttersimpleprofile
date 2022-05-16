78c22b099cec85e5b82f8b4c8fc2ed240df4ef4e
Bytx-youtube <80635487+Bytx-youtube@users.noreply.github.com>
2022-05-16 20:24:02 +0800

    Add files via upload

---
+++ lib/profilescreen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffdee2fe),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Column(
+                children: [
+                  Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(
+                      top: 100,
+                      bottom: 8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(2, 2),
+                          blurRadius: 10,
+                        ),
+                      ],
+                      image: const DecorationImage(
+                        image: AssetImage(
+                          "assets/images/dog.jpg",
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    "Kyle",
+                    style: TextStyle(
+                      fontFamily: "Montserrat",
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const Text(
+                    "Programmer",
+                    style: TextStyle(
+                      fontFamily: "Montserrat",
+                      color: Colors.black54,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  left: 24,
+                  right: 24,
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "PROFILE",
+                      style: TextStyle(
+                        fontFamily: "Montserrat",
+                        color: Colors.black87,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 16,),
+                    listProfile(Icons.person, "Full Name", "Kyle Anderson"),
+                    listProfile(Icons.date_range, "Date of Birth", "July 21, 2000"),
+                    listProfile(Icons.location_pin, "Location", "California, United States"),
+                    listProfile(Icons.male, "Gender", "Male"),
+                    listProfile(Icons.phone, "Phone Number", "15555228"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget listProfile(IconData icon, String text1, String text2) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(top: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 20,
+          ),
+          const SizedBox(width: 24,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                text1,
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontFamily: "Montserrat",
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                text2,
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontFamily: "Montserrat",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+}
